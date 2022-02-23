bd8a951b4338cf4fa64d24afa737ace12abea48a
takuumi <satu_noone@hotmail.com>
2022-02-23 10:58:37 +0900

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Takumi Sakamoto on 2022/02/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
