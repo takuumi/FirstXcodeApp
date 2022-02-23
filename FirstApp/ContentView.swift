2c59d7dde15e5ea9fde6544981f253668235c6cd
takuumi <satu_noone@hotmail.com>
2022-02-23 12:20:24 +0900

    練習。

--- FirstApp/ContentView.swift
+++ FirstApp/ContentView.swift
@@ -9,8 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("やどドヤァ!")
             .padding()
+            .frame(height: 2.0)
     }
 }
 
